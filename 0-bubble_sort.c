2dec3f91c36bb0b8773d1dee6213f7b1f07145b7
marc habib <54110010+OPShredder@users.noreply.github.com>
2024-01-14 21:20:42 +0200

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,36 +8,30 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int temp;
-    int swapped;
-
-    if (array == NULL || size < 2)
-        return;
-
-    for (i = 0; i < size - 1; i++)
-    {
-        swapped = 0;
-
-        for (j = 0; j < size - i - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                /* Swap the elements */
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-
-                /* Set swapped flag to true */
-                swapped = 1;
-
-                /* Print array after each swap */
-                print_array(array, size);
-            }
-        }
-
-        /* If no two elements were swapped, the array is already sorted */
-        if (swapped == 0)
-            break;
-    }
+size_t i, j;
+int temp;
+int swapped;
+if (array == NULL || size < 2)
+return;
+for (i = 0; i < size - 1; i++)
+{
+swapped = 0;
+for (j = 0; j < size - i - 1; j++)
+{
+if (array[j] > array[j + 1])
+{
+/* Swap the elements */
+temp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = temp;
+ /* Set swapped flag to true */
+swapped = 1;
+/* Print array after each swap */
+print_array(array, size);
+}
+}
+/* If no two elements were swapped, the array is already sorted */
+if (swapped == 0)
+break;
+}
 }
